8917d6a64af554f1f778b22a20018c7897c24884
Daniel Hines <d4hines@gmail.com>
2022-04-23 09:34:06 -0400

    add home-manager

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-generators }:
+  outputs = { self, nixpkgs, nixos-generators, home-manager }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -18,6 +22,15 @@
         ./nix/gnome.nix
         ./nix/nix-config.nix
         ./nix/nixpkgs-config.nix
+        home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.users.demo = { ... }: {
+                imports = [
+                  ./nix/home.nix
+                ];
+              };
+            }
         ./nix/packages.nix
       ];
 
